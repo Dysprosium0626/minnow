44656f58e41cfe6d994d7f6305748c69e4bcc910
Yue Deng <dysprosium0626@gmail.com>
2025-06-27 08:13:20 +0000

    fix: fix compile issue

--- src/network_interface.hh
+++ src/network_interface.hh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "address.hh"
+#include "arp_message.hh"
 #include "ethernet_frame.hh"
 #include "ipv4_datagram.hh"
 
@@ -94,7 +95,7 @@ private:
   std::map<uint32_t, size_t> arp_request_timestamps_;
 
   // Current time in milliseconds
-  size_t current_time_ms_ {0};
+  size_t current_time_ms_ { 0 };
   uint32_t arp_request_timeout_ms_ { 5000 };          // 5 seconds
   uint32_t arp_table_broadcast_timeout_ms_ { 30000 }; // 30 seconds
 };
